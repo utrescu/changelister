f5cdec2254a4fe2d643b99314bad3d73307efdbb
Xavier Sala <utrescu@gmail.com>
2025-10-06 17:17:17 +0200

    refactor: Processa els commits en el mòdul

---
+++ commits/commits.go
@@ -0,0 +1,128 @@
+package commits
+
+import (
+	"io"
+	"log"
+	"regexp"
+	"slices"
+	"strings"
+
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/utrescu/changelister/tags"
+)
+
+type CommitData struct {
+	Type      string
+	Header    string
+	Scope     string
+	Body      string
+	Author    string
+	DateTime  string
+	Important bool
+	Group     string
+}
+
+type ChangelogData struct {
+	Tag     string
+	Message string
+	Date    string
+	Commits map[string][]CommitData
+}
+
+func ProcessTagCommits(repo *git.Repository, tags tags.TagInfo, commitTypes []string) ChangelogData {
+
+	chIter, err := repo.Log(&git.LogOptions{
+		From:  tags.Stop,
+		Order: git.LogOrderCommitterTime,
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer chIter.Close()
+
+	logs := make(map[string][]CommitData)
+
+	for {
+		commit, err := chIter.Next()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatalf("Error iterant commits: %v", err)
+		}
+
+		// Si arribem al commit antic, parem
+		if commit.Hash == tags.Start {
+			break
+		}
+
+		log, valid := ProcessMessage(commit, commitTypes)
+		if valid {
+			if currentLogs, exists := logs[log.Group]; !exists {
+				logs[log.Group] = []CommitData{log}
+			} else {
+				currentLogs = append(currentLogs, log)
+				logs[log.Group] = currentLogs
+			}
+		}
+	}
+
+	changelog := ChangelogData{
+		Tag:     tags.Name,
+		Message: tags.Message,
+		Date:    tags.Date,
+		Commits: logs,
+	}
+	return changelog
+}
+
+func ProcessMessage(commit *object.Commit, commitTypes []string) (CommitData, bool) {
+	newmessage := strings.Trim(commit.Message, " ")
+	newmessage = strings.TrimSuffix(newmessage, "\n")
+
+	data, valid := ProcessMessageAndValidate(newmessage, commitTypes)
+	if valid {
+		if slices.Contains(commitTypes, data.Type) {
+			data.Author = commit.Author.Name
+			data.DateTime = commit.Author.When.String()
+			return *data, true
+		}
+	}
+	return CommitData{}, false
+}
+
+func ProcessMessageAndValidate(message string, commitTypes []string) (*CommitData, bool) {
+
+	groupCommitTypes := "(" + strings.Join(commitTypes, "|") + ")"
+	re := regexp.MustCompile(`(?m)` + groupCommitTypes + `\s*(\(.+\))?(!)?:(.*)`)
+
+	match := re.FindStringSubmatch(message)
+
+	if match != nil {
+		data := &CommitData{
+			Type:      match[1],
+			Header:    strings.TrimSuffix(match[4], "\n"),
+			Scope:     match[2],
+			Body:      "",
+			Important: match[3] == "!",
+			Author:    "",
+			DateTime:  "",
+		}
+
+		switch data.Type {
+		case "doc", "feat":
+			data.Group = "added"
+		case "fix":
+			data.Group = "fixed"
+		case "refactor":
+			data.Group = "changed"
+		case "chore", "ci", "build":
+			data.Group = "other"
+		}
+
+		return data, true
+	}
+	return nil, false
+}
--- main.go
+++ main.go
@@ -1,37 +1,14 @@
 package main
 
 import (
-	"io"
 	"log"
-	"regexp"
-	"slices"
-	"strings"
 
 	"github.com/go-git/go-git/v5"
-	"github.com/go-git/go-git/v5/plumbing/object"
+	commits "github.com/utrescu/changelister/commits"
+	config "github.com/utrescu/changelister/config"
 	tags "github.com/utrescu/changelister/tags"
 	template "github.com/utrescu/changelister/template"
-	config "github.com/utrescu/changelister/config"
 )
-
-type CommitData struct {
-	Type      string
-	Header    string
-	Scope     string
-	Body      string
-	Author    string
-	DateTime  string
-	Important bool
-	Group     string
-}
-
-type ChangelogData struct {
-	Tag     string
-	Message string
-	Date    string
-	Commits map[string][]CommitData
-}
-
 
 func main() {
 
@@ -50,113 +27,16 @@ func main() {
 		return
 	}
 
-	changelogData := []ChangelogData{}
+	changelogData := []commits.ChangelogData{}
 
 	for _, tags := range listTags {
 
 		// Processar les dades de l'etiqueta
-		processedTag := ProcessTagCommits(repo, tags, config.CommitTypes.Show)
+		processedTag := commits.ProcessTagCommits(repo, tags, config.CommitTypes.Show)
 		changelogData = append(changelogData, processedTag)
 
 	}
 
 	template.ProcessTemplate("changelog.tmpl", "changelog.md", changelogData)
 
-}
-
-func ProcessTagCommits(repo *git.Repository, tags tags.TagInfo, commitTypes []string) ChangelogData {
-
-	chIter, err := repo.Log(&git.LogOptions{
-		From:  tags.Stop,
-		Order: git.LogOrderCommitterTime,
-	})
-	if err != nil {
-		panic(err)
-	}
-	defer chIter.Close()
-
-	logs := make(map[string][]CommitData)
-
-	for {
-		commit, err := chIter.Next()
-		if err == io.EOF {
-			break
-		}
-
-		if err != nil {
-			log.Fatalf("Error iterant commits: %v", err)
-		}
-
-		// Si arribem al commit antic, parem
-		if commit.Hash == tags.Start {
-			break
-		}
-
-		log, valid := ProcessMessage(commit, commitTypes)
-		if valid {
-			if currentLogs, exists := logs[log.Group]; !exists {
-				logs[log.Group] = []CommitData{log}
-			} else {
-				currentLogs = append(currentLogs, log)
-				logs[log.Group] = currentLogs
-			}
-		}
-	}
-
-	changelog := ChangelogData{
-		Tag:     tags.Name,
-		Message: tags.Message,
-		Date:    tags.Date,
-		Commits: logs,
-	}
-	return changelog
-}
-
-func ProcessMessage(commit *object.Commit, commitTypes []string) (CommitData, bool) {
-	newmessage := strings.Trim(commit.Message, " ")
-	newmessage = strings.TrimSuffix(newmessage, "\n")
-
-	data, valid := ProcessMessageAndValidate(newmessage, commitTypes)
-	if valid {
-		if slices.Contains(commitTypes, data.Type) {
-			data.Author = commit.Author.Name
-			data.DateTime = commit.Author.When.String()
-			return *data, true
-		}
-	}
-	return CommitData{}, false
-}
-
-func ProcessMessageAndValidate(message string, commitTypes []string) (*CommitData, bool) {
-
-	groupCommitTypes := "(" + strings.Join(commitTypes, "|") + ")"
-	re := regexp.MustCompile(`(?m)` + groupCommitTypes + `\s*(\(.+\))?(!)?:(.*)`)
-
-	match := re.FindStringSubmatch(message)
-
-	if match != nil {
-		data := &CommitData{
-			Type:      match[1],
-			Header:    strings.TrimSuffix(match[4], "\n"),
-			Scope:     match[2],
-			Body:      "",
-			Important: match[3] == "!",
-			Author:    "",
-			DateTime:  "",
-		}
-
-		switch data.Type {
-		case "doc", "feat":
-			data.Group = "added"
-		case "fix":
-			data.Group = "fixed"
-		case "refactor":
-			data.Group = "changed"
-		case "chore", "ci", "build":
-			data.Group = "other"
-		}
-
-		return data, true
-	}
-	return nil, false
 }
